2016eb0991beda0a4e9504ebc020e9f69ae0b347
ThomasHarb <harbonnier.th@gmail.com>
2024-02-22 17:35:23 +0100

    Add the second project

--- src/views/landingPages/Home/components/Seeprojects/Seeprojects.js
+++ src/views/landingPages/Home/components/Seeprojects/Seeprojects.js
@@ -2,7 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
-import { LuBrainCircuit } from "react-icons/lu";
+/* import { LuBrainCircuit } from "react-icons/lu"; */
 import CardActions from "@mui/material/CardActions";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -10,6 +10,7 @@ import Avatar from "@mui/material/Avatar";
 import { FaChartArea } from "react-icons/fa";
 import Grid from "@mui/material/Grid";
 import { TbBrandJavascript } from "react-icons/tb";
+import { MdSpaceDashboard } from "react-icons/md";
 
 const Seeprojects = () => {
   return (
@@ -153,14 +154,14 @@ const Seeprojects = () => {
                 </Box>
                 <CardContent>
                   <Typography variant="h5" gutterBottom>
-                    Data Analysis of all movies in my Cloud
+                    Data Analysis of all movies on Netflix
                   </Typography>
                   <Typography
                     variant="body2"
                     component="div"
                     color="text.secondary"
                   >
-                    Analysis or all movie on Netflix. The dataset has been
+                    Analysis or all movies on Netflix. The dataset has been
                     downloaded from Kaggle. The data will be processed in Python
                     then integrated in a MySQL database. The data will be then
                     display in PowerBI.
@@ -170,7 +171,11 @@ const Seeprojects = () => {
                     Languages and softwares used
                   </Typography>
                   <Typography variant="body2">
-                    Python - MySQL - PowerBi - Tableau - excel
+                    Python - MySQL - PowerBi - Tableau - excel - Javascript
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    <br />
+                    Data Manipulation - Data Visualization
                   </Typography>
                 </CardContent>
                 <CardActions>
@@ -185,6 +190,102 @@ const Seeprojects = () => {
             </Grid>
             <Grid item xs={12} sm={6} md={4} style={{ display: "flex" }}>
               <Card
+                sx={{
+                  maxWidth: 343,
+                  borderRadius: "20px",
+                  boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+                  transition: "0.3s",
+                }}
+              >
+                <Box sx={{ minWidth: 256 }}>
+                  <Box
+                    sx={{
+                      padding: `4px 24px 0`,
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <Avatar
+                      alt={"brand logo"}
+                      sx={(theme) => ({
+                        width: 48,
+                        height: 48,
+                        transform: "translateY(50%)",
+                        "& > img": {
+                          margin: 0,
+                          backgroundColor: "common.white",
+                        },
+                        [theme.breakpoints.up("sm")]: {
+                          width: 60,
+                          height: 60,
+                        },
+                      })}
+                    >
+                      <MdSpaceDashboard />
+                    </Avatar>
+                    <Typography
+                      sx={{
+                        textTransform: "uppercase",
+                        fontSize: 14,
+                        color: "grey.500",
+                        letterSpacing: "1px",
+                      }}
+                    >
+                      Project
+                    </Typography>
+                  </Box>
+                  <Box
+                    component="hr"
+                    sx={(theme) => ({
+                      backgroundColor: "grey.200",
+                      marginBottom: `${24 - 1}px`, // minus 1 due to divider height
+                      [theme.breakpoints.up("sm")]: {
+                        marginBottom: `${30 - 1}px`, // minus 1 due to divider height
+                      },
+                    })}
+                  />
+                </Box>
+                <CardContent>
+                  <Typography variant="h5" gutterBottom>
+                    Bioinformatic Dashboard for FastP tool
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    color="text.secondary"
+                    textAlign="justify"
+                  >
+                    FastP tool is a bioinformatic tool used to chech the
+                    integrity and the quality of sequences ( aka read ). The
+                    output of this tool is a json file that can be difficult to
+                    read for people that are not used to this kind of data. This
+                    dashboard provide a interactive to read those data with
+                    histogram and tables. All the dashboard has been created
+                    with Javascript.
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    <br />
+                    Languages and softwares used
+                  </Typography>
+                  <Typography variant="body2">
+                    Javascript - React - Material UI
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    <br />
+                    Data Visualization
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    href="https://thomasharb.github.io/fastp-project/"
+                  >
+                    Learn more
+                  </Button>
+                </CardActions>
+              </Card>
+              {/* <Card
                 sx={{
                   maxWidth: 343,
                   borderRadius: "20px",
@@ -268,7 +369,7 @@ const Seeprojects = () => {
                     Learn more
                   </Button>
                 </CardActions>
-              </Card>
+              </Card> */}
             </Grid>
             <Grid item xs={12} sm={6} md={4} style={{ display: "flex" }}>
               <Card
@@ -349,7 +450,9 @@ const Seeprojects = () => {
                     <br />
                     Languages and softwares used
                   </Typography>
-                  <Typography variant="body2">React - Javascript</Typography>
+                  <Typography variant="body2">
+                    React - Material UI - Javascript
+                  </Typography>
                 </CardContent>
                 <CardActions>
                   <Button size="small" href="/">
